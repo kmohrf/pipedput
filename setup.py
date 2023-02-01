fe3f4ece583389279fad948597345226fa1885f3
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2023-02-01 15:42:05 +0100

    fix: remove accidental inclusion of flask-mail v1.0.0 in dependency version range

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     ],
     install_requires=[
         "flask>=1.1.2,<3.0.0",
-        "flask-mail<=1.0.0",
+        "flask-mail<1.0.0",
         "jinja2",
     ],
     package_data={"": ["README.md"]},
